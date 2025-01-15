d9b6a757307785ddc6b4b8f4a03d4213095aa900
Mailson Peixe <mailson.peixe@gmail.com>
2025-01-15 17:22:42 -0300

    first commit

--- main.tf
+++ main.tf
@@ -2,3 +2,8 @@ resource "local_file" "foo" {
   content  = "foo!"
   filename = "${path.module}/foo.bar"
 }
+
+resource "local_file" "bar" {
+  content  = "bar!"
+  filename = "${path.module}/foo.bar"
+}
